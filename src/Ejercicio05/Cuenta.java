b55d7f5021bda9e0a5232ae5e506a94ac30ea4e0
Carlos Jalil <jalilzambrano@hotmail.com>
2023-03-16 08:41:11 -0500

    segundo

---
+++ src/Ejercicio05/Cuenta.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio05;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Cuenta {
+    
+}
