388d7348b5bc21235167c43dbf2ce995d4965870
Carlos Jalil Oficina <jalilzambrano@hotmail.com>
2023-03-17 11:42:38 -0500

    primer commit 17Mar

---
+++ src/Ejercicio08/Cadena.java
@@ -0,0 +1,40 @@
+/*
+ Realizar una clase llamada Cadena que tenga como atributos una frase (de tipo de
+String) y su longitud. En el main se creará el objeto y se le pedirá al usuario que ingrese
+una frase que puede ser una palabra o varias palabras separadas por un espacio en
+blanco y a través de los métodos set, se guardará la frase y la longitud de manera
+automática según la longitud de la frase ingresada. Deberá además implementar los
+siguientes métodos: 
+
+ */
+package Ejercicio08;
+
+/**
+ *
+ * @author cjalil
+ */
+public class Cadena {
+    private String frase;
+    private int longitud;
+
+    public Cadena() {
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = frase.length();
+    }
+ 
+    
+}
